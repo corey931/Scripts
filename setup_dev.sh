f73ad191ddb37f9f77cebcd728b6faba199d82db
Sebastian Simon <sese_simon@gmx.net>
2020-06-27 19:41:06 +0200

    Added Azure client

--- setup_dev.sh
+++ setup_dev.sh
@@ -25,6 +25,17 @@ sudo passwd
 az vm open-port --resource-group myResourceGroup --name $vm --port 3389
 
 # upgrade python
-sudo apt-get install python3.7
+sudo apt-get install python3.7 --yes
 sudo update-alternatives --config python3
 sudo apt install python-pip --yes
+
+# azure client: https://docs.microsoft.com/en-us/cli/azure/install-azure-cli-apt?view=azure-cli-latest
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+sudo apt-get update
+sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
+curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+sudo apt-get update
+sudo apt-get install azure-cli
+az login
