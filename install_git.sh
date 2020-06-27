386e0e681c95268fca928c8b880a18f73d1ab2f4
Sebastian Simon <sese_simon@gmx.net>
2020-06-27 18:04:22 +0200

    Added first git and main changes

---
+++ install_git.sh
@@ -0,0 +1,9 @@
+sudo apt-get install git-core
+git --version
+
+git config --global user.name "Sese"
+git config --global user.email "sese_simon@gmx.net"
+
+usr="AzureUser"
+mkdir /home/$usr/github
+cd /home/$usr/github
